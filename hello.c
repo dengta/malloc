5bc46dbeb24fdd54339eb9b76c02b6aa853266ed
liuqun <liuqiong1125@163.com>
2013-04-25 07:19:21 -0400

    Add hello.c file

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: liuqun
+	> Mail: liuqlzu@gmail.com 
+	> Created Time: Thu 25 Apr 2013 07:18:11 AM EDT
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+	printf("hello\n");
+	return 0;
+}
